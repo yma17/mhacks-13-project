028febe2c45750920c1803b5b4edf607d5e0049f
chloegoh <chloegoh@umich.edu>
2020-08-22 08:11:02 -0400

    Add chat route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyrebase
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 config = {
   "apiKey": "AIzaSyAQk0A5csVTIUIEfvdXXiXaelEG3OWes9U",
@@ -11,20 +11,40 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+# db.child("names").push({"name": "chloe"})
+
 # Initialize Flask App
 app = Flask(__name__)
+# socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/chat")
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route('/chat/', methods=['GET', 'POST'])
 def chat():
-    return render_template("chat.html")
+    if request.method == 'POST':
+        # print(db.child('messages').get().val())
+        # userId = request.form['userId']
+        message = request.form['message']
+        db.child("messages").push({"message": message})
+        # db.child("rooms").push({'room1': {"user": userId, "message": message}})
+    return render_template('chat.html')
+
 @app.route('/profile/')
 def about():
     print(db.get())
     return render_template('profile.html', data=db.get().val())
 
+# @socketio.on('send_message')
+# def handle_send_message_event(data):
+#     # notify all users
+#     socketio.emit("receive_message", data, room=data['room'])
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    # socketio.run(app)
--- static/chat/script.js
+++ static/chat/script.js
@@ -1,27 +1,13 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyAQk0A5csVTIUIEfvdXXiXaelEG3OWes9U",
-  authDomain: "mhacks-13-project.firebaseapp.com",
-  databaseURL: "https://mhacks-13-project.firebaseio.com",
-  projectId: "mhacks-13-project",
-  storageBucket: "mhacks-13-project.appspot.com",
-  messagingSenderId: "999121062859",
-  appId: "1:999121062859:web:c8e7a76b85aafa8eb2844e",
-  measurementId: "G-PS38VSCWL3",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var name = prompt("Enter your name");
-
 function sendMessage() {
-  // get message
+  // get messages div
+  var messages = document.getElementById("messages");
+
+  // get new message
   var message = document.getElementById("message").value;
 
-  // save in database
-  //   firebase
-  //     .database()
-  //     .ref("messages")
-  //     .push()
-  //     .set({ sender: name, message: message });
+  // display new message on screen
+  var newNode = document.createElement("P");
+  newNode.innerText = message;
+
+  messages.appendChild(newNode);
 }
