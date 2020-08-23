96f57988fdb2e18ed04f8cbe5a5ad2bc87c6d83a
chloegoh <chloegoh@umich.edu>
2020-08-23 08:51:53 -0400

    Add design to chat page

--- static/chat/script.js
+++ static/chat/script.js
@@ -1,13 +1,27 @@
 function sendMessage() {
-  // get messages div
+  // // get messages div
+  // var messages = document.getElementById("messages");
+
+  // // get new message
+  // var message = document.getElementById("message").value;
+
+  // // display new message on screen
+  // var newNode = document.createElement("P");
+  // newNode.innerText = message;
+
+  // messages.appendChild(newNode);
+
+  // get messages container
   var messages = document.getElementById("messages");
 
   // get new message
   var message = document.getElementById("message").value;
 
-  // display new message on screen
-  var newNode = document.createElement("P");
-  newNode.innerText = message;
+  if (message) {
+    // display new message on screen
+    var newNode = document.createElement("TR");
+    newNode.innerHTML = `<td class="text-right" style="border-style: none;"><span>${message} : </span><strong>You</strong><img class="rounded-circle mr-2" width="30" height="30" src="/static/assets/img/avatars/avatar4.jpeg"></td>`;
 
-  messages.appendChild(newNode);
+    messages.appendChild(newNode);
+  }
 }
